fc6bf3d21fffe0fd5359c0c5db84980ba2b31489
Erika Doyle Navara <erika.doyle@microsoft.com>
2016-04-27 23:12:21 -0700

    Whoops, this didn't get included in the last commit

--- database_setup.py
+++ database_setup.py
@@ -29,6 +29,7 @@ class Category(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     image = Column(String(255))
+    user_id = Column(Integer, ForeignKey('user.id', onupdate="CASCADE", ondelete="SET NULL"), nullable=True)
     user = relationship(User)
 
     @property
@@ -52,7 +53,9 @@ class Species(Base):
     description = Column(String(250))
     image = Column(String(255))
     caption = Column(String(30))
+    category_id = Column(Integer, ForeignKey('category.id', onupdate="CASCADE", ondelete="SET NULL"), nullable=True)
     category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id', onupdate="CASCADE", ondelete="SET NULL"), nullable=True)
     user = relationship(User)
 
 
